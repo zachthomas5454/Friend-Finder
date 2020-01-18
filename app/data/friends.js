5c2ff7e0c7c1e138a3335b079e26fbe9edc931cd
Zach <zachariahthomas54@yahoo.com>
2020-01-18 18:15:55 -0500

    Changes in HTML and JS files

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Salma",
+		photo: "image.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Sarah",
+		photo: "image.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Penelope",
+		photo: "image.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Alisson",
+		photo: "image.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Kristina",
+		photo: "image.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
