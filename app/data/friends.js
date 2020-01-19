e819a47026a5debb7212ff137bdef08dff35bca1
Zach <zachariahthomas54@yahoo.com>
2020-01-18 19:45:49 -0500

    Final Changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friends = [
 	{
 		name: "Salma",
-		photo: "image.jpg",
+		photo: "https://www.stylist.co.uk/images/app/uploads/2019/09/03104935/salma-hayek-crop-1567504185-1129x1129.jpg?w=256&h=256&fit=max&auto=format%2Ccompress",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
 	},
 	{
 		name: "Sarah",
-		photo: "image.jpg",
+		photo: "https://pbs.twimg.com/profile_images/743137985332396032/qY4JDKJi_400x400.jpg",
 		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
 		name: "Penelope",
-		photo: "image.jpg",
+		photo: "https://pbs.twimg.com/profile_images/3440706620/4a48450d0653c234df06d301bf28f0cb.jpeg",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
 	},
 	{
-		name: "Alisson",
-		photo: "image.jpg",
+		name: "Jennifer",
+		photo: "https://pbs.twimg.com/profile_images/429010555836174336/gUrccdi3.jpeg",
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 	{
-		name: "Kristina",
-		photo: "image.jpg",
+		name: "Kelly",
+		photo: "https://pbs.twimg.com/profile_images/3187766559/b74b29bf0f57409a9d6e525fe838bcf1.jpeg",
 		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
 	}
 ];
